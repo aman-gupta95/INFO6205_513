925900342e1e7ee41e70d0f9eb99096d0a598fcf
Aman Gupta <gupta.am@husky.neu.edu>
2019-04-13 01:12:33 -0400

    Created a genotype class for the implementation

---
+++ src/Main/SeeMidi.java
@@ -0,0 +1,58 @@
+package Main;
+
+import java.io.File;
+
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+public class SeeMidi {
+    public static final int NOTE_ON = 0x90;
+    public static final int NOTE_OFF = 0x80;
+    public static final String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
+    public static void main(String[] args) throws Exception {
+        Sequence sequence = MidiSystem.getSequence(new File("C:\\Users\\amans\\Documents\\INFO6205\\Final Project\\INFO6205_513\\src\\Resources\\looperman-l-0672759-0160615-chill-tabla-102.wav.mid"));
+
+        int trackNumber = 0;
+        for (Track track :  sequence.getTracks()) {
+            trackNumber++;
+            System.out.println("Track " + trackNumber + ": size = " + track.size());
+            System.out.println();
+            for (int i=0; i < track.size(); i++) {
+                MidiEvent event = track.get(i);
+                System.out.print("@" + String.valueOf(event.getTick()) + " tickLength: "+String.valueOf(sequence.getResolution()));
+                MidiMessage message = event.getMessage();
+                if (message instanceof ShortMessage) {
+                    ShortMessage sm = (ShortMessage) message;
+                    System.out.print("Channel: " + sm.getChannel() + " ");
+                    if (sm.getCommand() == NOTE_ON) {
+                        int key = sm.getData1();
+                        int octave = (key / 12)-1;
+                        int note = key % 12;
+                        String noteName = NOTE_NAMES[note];
+                        int velocity = sm.getData2();
+                        System.out.println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
+                    } else if (sm.getCommand() == NOTE_OFF) {
+                        int key = sm.getData1();
+                        int octave = (key / 12)-1;
+                        int note = key % 12;
+                        String noteName = NOTE_NAMES[note];
+                        int velocity = sm.getData2();
+                        System.out.println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity);
+                    } else {
+                        System.out.println("Command:" + sm.getCommand());
+                    }
+                } else {
+                    System.out.println("Other message: " + message.getClass());
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+}
