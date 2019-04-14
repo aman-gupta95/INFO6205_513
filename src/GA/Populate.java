087eff0ba7f7c53807c50476b9fc7ad26cff71e2
Aman Gupta <gupta.am@husky.neu.edu>
2019-04-14 18:44:57 -0400

    Created mutation class to generate random genes

--- src/GA/Populate.java
+++ src/GA/Populate.java
@@ -3,12 +3,12 @@ package GA;
 import java.util.ArrayList;
 
 public class Populate {
-    public static ArrayList<Individual> initPool(int poolSize, int popSize, int maxKey, int maxVelocity){
+    public static ArrayList<Individual> initPool(int poolSize, int popSize, int maxKey, int maxVelocity, int maxTick){
         ArrayList<Individual> soundPopulation = new ArrayList<>();
         for(int i = 0; i < poolSize; i++){
             ArrayList<Genotype> genes = new ArrayList<>();
             for(int j = 0; j < popSize; j++){
-                genes.add();
+                genes.add(Mutation.getRandomGenes(maxKey, maxVelocity, maxTick));
             }
             soundPopulation.add(new Individual(genes));
         }
