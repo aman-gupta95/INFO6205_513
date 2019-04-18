6b69959c6fe4a8cb35afb4dac4c6e55980ae6d0c
Vishnu Bhargavi Sabbisetty <bhargavi_sabbisetty@yahoo.in>
2019-04-18 16:50:59 -0400

    Implemented cross-over where two best individuals are selected and new indivial is generated by randomly selecting bits of two individuals

---
+++ src/GA/CrossOver.java
@@ -0,0 +1,62 @@
+package GA;
+import java.util.ArrayList;
+import java.util.Random;
+public class CrossOver {
+    private static Individual crossOver(ArrayList<Individual> newPool, int best, int maxKey, int maxVelocity, int mutationCount,int goal_length) {
+        // get random 2 from elite
+        Random random = new Random();
+        int first = random.nextInt(best);
+        int second = random.nextInt(best);
+        Individual individual = null;
+        while (first == second) {
+            second = random.nextInt(best);
+        }
+        // mate the two
+        switch (random.nextInt(2)) {
+            case 0: {//just randomly shuffle
+                individual = crossOver(newPool.get(first), newPool.get(second), mutationCount, maxKey, maxVelocity);
+            }
+            case 1: {//generate by random index
+//                individual = crossOver(newPool.get(first), newPool.get(second),mutationCount, maxKey, maxVelocity, random.nextInt(goal_length));
+            }
+        }
+        return individual;
+    }
+
+
+    public static Individual crossOver(Individual first, Individual second, int mutationCount, int maxKey, int maxVelocity) {
+        Random random = new Random();
+        ArrayList<Genotype> genes = new ArrayList<>();
+        Individual individual = new Individual(genes);
+        for (int i = 0; i < first.getIndividual().size(); i++) {
+            switch (random.nextInt(2)) {
+                case 0: {
+                    individual.getIndividual().add(new Genotype(first.getIndividual().get(i)));
+                    break;
+                }
+                case 1: {
+                    individual.getIndividual().add(new Genotype(second.getIndividual().get(i)));
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < mutationCount; i++) {
+            Mutation.mutate(genes, maxKey, maxVelocity);
+        }
+        return individual;
+    }
+
+
+    public static ArrayList<Individual> mateBest(ArrayList<Individual> pool, int best, int goal_length, int mutationCount, int maxKey, int maxVelocity) {
+        ArrayList<Individual> newPool = new ArrayList<>();
+        for (int i = 0; i < pool.size(); i++) {
+            if (i < best) {
+                newPool.add(pool.get(i));
+            } else {
+                newPool.add(crossOver(newPool, best, maxKey, maxVelocity, mutationCount,goal_length));
+            }
+        }
+        return newPool;
+    }
+}
