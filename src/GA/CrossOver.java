bcbd6c94c27ed128f95830eb162055c2f6154f4f
Vishnu Bhargavi Sabbisetty <bhargavi_sabbisetty@yahoo.in>
2019-04-18 16:52:53 -0400

    A cross-over where a random index is generated where part before index is taken from first part and after index is taken from second part

--- src/GA/CrossOver.java
+++ src/GA/CrossOver.java
@@ -17,7 +17,7 @@ public class CrossOver {
                 individual = crossOver(newPool.get(first), newPool.get(second), mutationCount, maxKey, maxVelocity);
             }
             case 1: {//generate by random index
-//                individual = crossOver(newPool.get(first), newPool.get(second),mutationCount, maxKey, maxVelocity, random.nextInt(goal_length));
+                individual = crossOver(newPool.get(first), newPool.get(second),mutationCount, maxKey, maxVelocity, random.nextInt(goal_length));
             }
         }
         return individual;
@@ -47,6 +47,23 @@ public class CrossOver {
         return individual;
     }
 
+    public static Individual crossOver(Individual first, Individual second, int mutationCount, int maxKey, int maxVelocity, int goal_length) {
+        Random random = new Random();
+        ArrayList<Genotype> genes = new ArrayList<>();
+        Individual individual = new Individual(genes);
+        int temp = random.nextInt(goal_length);
+        for (int i = 0; i < temp; i++) {
+            individual.getIndividual().add(new Genotype(first.getIndividual().get(i)));
+        }
+        for (int i = temp; i < goal_length; i++) {
+            individual.getIndividual().add(new Genotype(second.getIndividual().get(i)));
+        }
+
+        for (int i = 0; i < mutationCount; i++) {
+            Mutation.mutate(genes, maxKey, maxVelocity);
+        }
+        return individual;
+    }
 
     public static ArrayList<Individual> mateBest(ArrayList<Individual> pool, int best, int goal_length, int mutationCount, int maxKey, int maxVelocity) {
         ArrayList<Individual> newPool = new ArrayList<>();
